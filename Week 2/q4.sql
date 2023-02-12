98162c38cd2ee938e5232f82b08e1da3d20bb049
Aashna J Chandrashekhar <aashnajc@gmail.com>
2023-02-12 21:44:26 +0530

    ,

---
+++ Week 2/q4.sql
@@ -0,0 +1,4 @@
+/* Q) Create a table MANAGER with the columns mgr-id, name, salary and hiredate */
+
+CREATE TABLE manager(mgr-id number(5) primary key,
+name varchar(20), sal number(5), hiredate date);
