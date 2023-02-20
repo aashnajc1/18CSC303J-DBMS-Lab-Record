7f2e9a5a8518be6eb4fd10b54ef728bdf5be56cd
Aashna J Chandrashekhar <aashnajc@gmail.com>
2023-02-20 21:52:25 +0530

    ,

---
+++ Week 3/q20.sql
@@ -0,0 +1,3 @@
+/* q) List ename, deptno and sal after sorting emp table in ascending order by deptno and then descending order by sal. */
+
+select ename,deptno,sal from emp order by deptno asc, salary desc;
