4ebf3bf4a2a8d74b056cd4105f143502d08f85c1
Aashna J Chandrashekhar <aashnajc@gmail.com>
2023-03-18 23:10:07 +0530

    ,

---
+++ Week 4/q8.sql
@@ -0,0 +1,5 @@
+/* Q) Insert data into the table which satisfies the constraints on DEPT table first.*/
+
+insert into department values(1,'Aashna','Hyderabad);
+insert into department values(2, 'Shreya', 'Mumbai');
+insert into department values(3, 'Sonal', 'Chennai');
