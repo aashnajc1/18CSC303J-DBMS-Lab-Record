4893d8a753c9de8ed50524dfa6dffc4afabf3328
Aashna J Chandrashekhar <aashnajc@gmail.com>
2023-04-11 13:51:09 +0530

    ,

---
+++ Week 5/q1.sql
@@ -0,0 +1,4 @@
+/* Give grant permission to your neighbor for any one of your tables. Tell him/her
+to access (modify the data) your table from their login. */
+
+grant all on department to RA2011027010033;
