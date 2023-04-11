fbb0f12cc7858d67996e0ea2593caac4d60964a7
Aashna J Chandrashekhar <aashnajc@gmail.com>
2023-04-11 13:56:10 +0530

    ,

---
+++ Week 5/q4.sql
@@ -0,0 +1,8 @@
+/* Create the department table add the details and commit the data. */
+
+create table dept(deptno number(5), dname varchar(20), loc varchar(20));
+insert into dept values(10, 'Accounting','New York');
+insert into dept values(20, 'Research','Dallas');
+insert into dept values(30, 'Sales','Chicago');
+insert into dept values(40, 'Operation','Boston');
+insert into dept values(50, 'Manufacturing','Boston');
